fb6f76bf5453629c171d3c7ee0f4653956b04855
Raja-Srinivas <94114452+Raja-Srinivas@users.noreply.github.com>
2024-12-06 16:49:42 +0530

    order-processing-commmits

---
+++ src/test/java/com/pid/com/OrderProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pid.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
